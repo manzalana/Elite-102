ac6efec9e2c269a48a7cf66d4d8bedb482748c7f
Manzlana <manzanaresalana1@gmail.com>
2024-04-08 18:45:04 -0500

    Account validation with account number and PIN

--- main.py
+++ main.py
@@ -5,36 +5,66 @@ cursor = connection.cursor()
 #welcome
 print('Welcome to this real cool bank!')
 
+def getAccountNumber():
+    #ask for Account Number
+    accNum= input('Please Enter Your Accout Number: ')
+    accFound=False
 
-#ask for Account Number
-accNum= input('Please Enter Your Accout Number: ')
+    #get all account numbers, then loop through them to see if this is valid. if not then uhoh
+    accNumValidationQuery=("SELECT AccountNumber FROM guests")
+    cursor.execute(accNumValidationQuery)
+    allAccNum=cursor.fetchall()
+    for accounts  in allAccNum:
+        
+        #checks if the account number entered is in the system
+        if int(accounts[0]) ==int(accNum):
+            print(accounts[0])
+            #if it is found then this will run
+            accFound=True
+    #if there isnt an account found, the userLogin function will loop
+    if (accFound==False):
+        print('Account not Found. Please Try Again')
+        getAccountNumber()
+    
+    print('Account Found!')
+    
 
-#ask for accout PIN for that account number
-pinNum= input('Please Enter Your PIN: ')
+    #ask for the pin number
+    pinNum= input('Please Enter Your PIN: ')
+
+    #get the correct PIN number
+    pinQuery= ("SELECT Id FROM guests WHERE AccountNumber="+accNum)
+    cursor.execute(pinQuery)
+    userPin=cursor.fetchone()
+   
+    #see if the PIN's match up
+
+    if(int(pinNum)==int(userPin[0])):
+        print('PIN Correct')
+    else:
+        #if PIN is Incorrect, this will run
+        print('Incorrect PIN. Please re-enter your Account Number')
+        getAccountNumber()
+    #testQuery = ("SELECT Id FROM guests WHERE AccountNumber="+accNum)
+    
+    #menu
+    # Check balance, withdrawl money, deposite money
+    print('Choose an option from this menu')
 
 
-#menu
-# Check balance, withdrawl money, deposite money
-print('Choose an option from this menu')
+
+    #for item in cursor:
+
+        #print(item)
+
+    
+   
+    
 
 
+getAccountNumber()
 
-
-testQuery = ("SELECT Id FROM guests WHERE AccountNumber="+accNum)
-
- 
-
-cursor.execute(testQuery)
-plsid=cursor.fetchone()
-
-#for item in cursor:
-
-    #print(item)
-
- 
-print(plsid[0])
 cursor.close()
-
 connection.close()
 
 
