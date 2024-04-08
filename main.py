e296705874930c7ad7f112c1aa45647876037e2e
Manzlana <manzanaresalana1@gmail.com>
2024-04-08 17:07:12 -0500

    Put the insturctions of the project in the code

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ connection.commit()
 cursor.close()
 
 connection.close()
+
+
+#In this project you will create an online banking program. 
+#Users need to have an account number and PIN to identify themselves 
+#as owners of an account. Once users get into the system they will have 
+#standard options: check balance, deposit, and withdraw. Additionally, a 
+#new user or bank administrator can also create a new account, close 
+#account, and modify an account (such as edit name, PIN, or any other 
+#personal identification required to open an account).
+#
+#
